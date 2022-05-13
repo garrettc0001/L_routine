fcd40c9809cfc4b04d731c9c13f71948d3eff4fa
Garrett Curtis <garrettc0001@gmail.com>
2022-05-13 08:38:03 -0400

    fixed drivetrain.py. Made first drive forward part.

--- drivetrain.py
+++ drivetrain.py
@@ -1,12 +1,14 @@
 import commands2
+
 import ctre
+
 
 
 class Drivetrain(commands2.SubsystemBase):
     def __init__(self):
         super().__init__()
         self.gyro = ctre.PigeonIMU(10)
-        self.gyro.setYaw(0,0)
+        self.gyro.setYaw(0, 0)
         
         self.m_left = ctre.TalonSRX(0)
         self.m_left_2 = ctre.VictorSPX(1)
@@ -21,8 +23,8 @@ class Drivetrain(commands2.SubsystemBase):
         self.m_right_2.follow(self.m_right)
         self.m_right_3.follow(self.m_right)
         
-        self.m_right_encoder=ctre.CANCoder(9)
-        self.m_left_encoder=ctre.CANCoder(11)
+        self.m_right_encoder = ctre.CANCoder(9)
+        self.m_left_encoder = ctre.CANCoder(11)
 
     def set(self, left: float, right: float):
         self.m_left.set(ctre.ControlMode.PercentOutput, left)
--- robot.py
+++ robot.py
@@ -4,16 +4,18 @@ from wpimath.controller import PIDController
 from drivetrain import Drivetrain
 
 
-class Robot(wpilib.TimedRobot):
+class Robot(TimedRobot):
     
-    joy1=Joystick(0)
+    joy1 = Joystick(0)
     
     def __init__(self):
         super().__init__()
         self.drivetrain = Drivetrain()
 
     def robotInit(self):
-        pass
+        self.wheel_diameter = 4  # inches
+        self.drive_controller = PIDController(.001, 0, 0)
+        self.turn_controller = PIDController(.001, 0, 0)
 
     def robotPeriodic(self):
         pass
@@ -28,11 +30,28 @@ class Robot(wpilib.TimedRobot):
         self.drivetrain.set(speed+turn, speed-turn)
 
     def autonomousInit(self):
-        pass
+        self.drivetrain.gyro.setYaw(0)
+        self.drivetrain.m_right_encoder.setPosition(0)
+        self.drivetrain.m_left_encoder.setPosition(0)
+        self.turn_controller.enableContinuousInput(0, 360)
 
     def autonomousPeriodic(self):
+        self.drive_controller.setSetpoint(1 * 12 / (math.pi * self.wheel_diameter) * 360)
+        # if not self.drive_controller.atSetpoint():
+        motion = self.drive_controller.calculate(measurement= self.drivetrain.m_right_encoder.getPosition())
+        self.drivetrain.set(motion, motion)
         pass
+        # self.turn_controller.setSetpoint(90)
+        # if not self.turn_controller.atSetpoint():
+        #     motion = self.turn_controller.calculate(measurement= self.drivetrain.gyro.getYaw())
+        #     self.drivetrain.set(motion, - motion)
+        #     pass
+        # self.drive_controller.setSetpoint(1 * 6 / self.wheel_radius / math.pi * 360)
+        # if not self.drive_controller.atSetpoint():
+        #     motion = self.turn_controller.calculate(measurement=self.drivetrain.m_right_encoder.getPosition())
+        #     self.drivetrain.set(motion, motion)
+        #     pass
 
 
 if __name__ == "__main__":
-    wpilib.run(Robot)
+    run(Robot)
